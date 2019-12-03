212337883851632bc44b0ac97195eb286abe5cb7
pedrocore73 <pjimenez@corenetworks.es>
2019-12-03 17:27:38 +0100

    users

--- app.js
+++ app.js
@@ -9,20 +9,35 @@ app.use(bodyParser.urlencoded({'extended':'false'}));
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
+let users = [];
+
 io.on('connection', socket => {
-    console.log('Un cliente se ha conectado');
+    
+    let user = new Object();
+    if(!users.some(user => user.id === socket.id) || users.length === 0) {
+        user.id = socket.id;
+
+        socket.on('start', data => {
+            user.nombre = (JSON.parse(data)).nombre;
+            users.push(user);
+            io.emit('mensaje', JSON.stringify(users));
+        })
+    }
+
+    console.log(users);
 
     socket.on('mensaje', data =>{
         console.log(data);
-        io.emit('mensaje', 'Conectado correctamente');
+        io.emit('mensaje', data);
     })
-
 
     socket.on('disconnect', ()=> {
         console.log('Un cliente se ha desconectado');
     })
-
+    console.log(users);
 })
+
+
 
 http.listen(3000, () => {
     console.log('Servidor ok en http://localhost:3000');
